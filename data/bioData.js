dbc104c5ad05e94287d7483913a2d63a5afab7d6
NongTeam <amteamxmail@gmail.com>
2024-12-22 15:11:18 +0700

    change des

--- data/bioData.js
+++ data/bioData.js
@@ -1,4 +1,4 @@
 export const bioData = {
-  descOne: `I'm a student who loves programming and got started back in high school.`,
-  descTwo: `I'm excited to keep learning, take on challenges, and grow my skills.`,
+  descOne: `Dynamic and results-driven front-end developer with 3 years of experience, specializing in building scalable, responsive, and user-friendly web and mobile applications. I have developed a strong foundation in HTML, CSS, JavaScript, Next.js, Flutter, and Dart, enabling me to deliver impactful results and drive success in diverse projects.`,
+  descTwo: `and I am committed to continuous learning and professional growth.`,
 };
